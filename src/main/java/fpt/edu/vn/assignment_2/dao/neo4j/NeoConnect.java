d0d214c4613513ffa35d31c083c8b95ef9bd3ab9
mkbvnh <mkbvnh@gmail.com>
2018-07-25 19:22:37 +0700

    final of final

--- src/main/java/fpt/edu/vn/assignment_2/dao/neo4j/NeoConnect.java
+++ src/main/java/fpt/edu/vn/assignment_2/dao/neo4j/NeoConnect.java
@@ -19,6 +19,65 @@ public class NeoConnect implements AutoCloseable {
     private final String COMMENT_BY = "COMMENT_BY";
     private final String RATE_BY = "RATE_BY";
 
+    //TEST
+
+//    public static void main(String[] args) {
+//        try {
+//            NeoConnect neo = new NeoConnect();
+//            //Create User
+//            neo.insertUser("5b5458d7a8b66c01648df938");//hieupm
+//            neo.insertUser("5b545907a8b66c370c7c8ce4");//baonq
+//            neo.insertUser("5b583c42cc29d812c0f376f2");//hoadx
+//            neo.insertUser("5b5856e0a8b66c351077081c");//acc1
+//            neo.insertUser("5b5856e2a8b66c3510770825");//acc2
+//            neo.insertUser("5b5856e2a8b66c3510770826");//acc3
+//            neo.insertUser("5b585bf0a8b66c351077082d");//acc4
+//            neo.insertUser("5b573d315198c415488cfec1");//minhnd
+//            System.out.println("Users Inserted");
+//            //Insert Doc
+//            neo.insertDocument("5b5629b0ebca4e28282ca407", "5b5458d7a8b66c01648df938");//hieupm
+//            neo.insertDocument("5b5772695198c42c94132d2f", "5b5458d7a8b66c01648df938");//hieupm
+//            neo.insertDocument("5b577a755198c42c94132d30", "5b545907a8b66c370c7c8ce4");//baonq
+//            System.out.println("Doc Inserted");
+//
+//            //invite Friends
+//            neo.makeFriends("5b545907a8b66c370c7c8ce4", "5b5458d7a8b66c01648df938");//bao vs hieu
+//            neo.makeFriends("5b545907a8b66c370c7c8ce4", "5b5458d7a8b66c01648df938");//bao vs hieu  //For Duplicated
+//            neo.makeFriends("5b5458d7a8b66c01648df938", "5b583c42cc29d812c0f376f2");//hieu - hoa
+//            neo.makeFriends("5b5458d7a8b66c01648df938", "5b573d315198c415488cfec1");//hieu - minh
+//            System.out.println("Friendship Added");
+//            neo.unFriend("5b5458d7a8b66c01648df938", "5b573d315198c415488cfec1");
+//            System.out.println("Unfriended");
+//
+//            //Comment
+//            neo.commentADocument(new Comment("5b545907a8b66c370c7c8ce4", "5b5629b0ebca4e28282ca407", "Hello Hieu", 0));//Time tu tao ben duoi
+//            neo.commentADocument(new Comment("5b5458d7a8b66c01648df938", "5b5629b0ebca4e28282ca407", "Hi Bao", 0));//Time tu tao ben duoi
+//            neo.commentADocument(new Comment("5b545907a8b66c370c7c8ce4", "5b5629b0ebca4e28282ca407", "Are u fine?", 0));//Time tu tao ben duoi
+//            System.out.println("Commented");
+//
+//            //Rate
+//            neo.rateADocument("5b545907a8b66c370c7c8ce4", "5b5629b0ebca4e28282ca407", 4);
+//            neo.rateADocument("5b5458d7a8b66c01648df938", "5b5629b0ebca4e28282ca407", 5);
+//            System.out.println("Rated");
+//
+//            //Like
+//            neo.likeADocument("5b5458d7a8b66c01648df938", "5b5629b0ebca4e28282ca407");
+//            neo.likeADocument("5b545907a8b66c370c7c8ce4", "5b5629b0ebca4e28282ca407");
+//            neo.likeADocument("5b583c42cc29d812c0f376f2", "5b5629b0ebca4e28282ca407");
+//            System.out.println("Liked");
+//            neo.unlikeADocument("5b583c42cc29d812c0f376f2", "5b5629b0ebca4e28282ca407");//Unlike
+//            System.out.println("Unliked");
+//            neo.likeADocument("5b583c42cc29d812c0f376f2", "5b5629b0ebca4e28282ca407");
+//            System.out.println("Liked");
+//            System.out.println("Demo functions successfully. Congatz!!!!!!!!!!!!!");
+//            //Close connection
+//
+//            neo.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
 
     public NeoConnect() {
         this.driver = GraphDatabase.driver(NEO4J_URI, AuthTokens.basic(NEO4J_NAME, NEO4J_PASSWORD));
@@ -26,16 +85,6 @@ public class NeoConnect implements AutoCloseable {
 
     public void close() throws Exception {
         driver.close();
-    }
-
-    public static void main(String[] args) {
-        try {
-            NeoConnect neo = new NeoConnect();
-            System.out.println("Neo Connected");
-            neo.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
     }
 
     //For Users
@@ -52,7 +101,10 @@ public class NeoConnect implements AutoCloseable {
     }
 
     // Create a friendship between two people.
-    private void makeFriends(String user1Id, String user2Id) {
+    private boolean makeFriends(String user1Id, String user2Id) {
+        if (isFriend(user1Id, user2Id)) {
+            return false;
+        }
         // Sessions are lightweight and disposable connection wrappers.
         try (Session session = driver.session()) {
             // Wrapping Cypher in an explicit transaction provides atomicity
@@ -66,6 +118,41 @@ public class NeoConnect implements AutoCloseable {
                                 "MATCH (b:User {id: $user_1}) " +
                                 "MERGE (a)-[:" + INVITE_FRIEND + "]->(b)",
                         Values.parameters("user_2", user2Id, "user_1", user1Id));
+                tx.success();  // Mark this write as successful.
+                return true;
+            }
+        }
+    }
+
+    //Check isFriend
+    private boolean isFriend(String userId1, String userId2) {
+        try (Session session = driver.session()) {
+            // Wrapping Cypher in an explicit transaction provides atomicity
+            // and makes handling errors much easier.
+
+            try (Transaction tx = session.beginTransaction()) {
+                StatementResult result = tx.run("MATCH (a:User)-[r:" + INVITE_FRIEND + "]->(b:User) " +
+                                "WHERE a.id = {userId1} AND b.id = {userId2} return r",
+                        Values.parameters("userId1", userId1, "userId2", userId2));
+                while (result.hasNext()) {
+                    return true;
+                }
+                tx.success();  // Mark this write as successful.
+            }
+        }
+        return false;
+    }
+
+    //Unfriend
+    private void unFriend(String userId1, String userId2) {
+        try (Session session = driver.session()) {
+            // Wrapping Cypher in an explicit transaction provides atomicity
+            // and makes handling errors much easier.
+            try (Transaction tx = session.beginTransaction()) {
+                tx.run("MATCH (a:User {id:{userId1}})-[r:" + INVITE_FRIEND + "]->(b:User {id:{userId2}}) DELETE r",
+                        Values.parameters("userId1", userId1, "userId2", userId2));
+                tx.run("MATCH (a:User {id:{userId2}})-[r:" + INVITE_FRIEND + "]->(b:User {id:{userId1}}) DELETE r",
+                        Values.parameters("userId2", userId2, "userId1", userId1));
                 tx.success();  // Mark this write as successful.
             }
         }
@@ -164,7 +251,7 @@ public class NeoConnect implements AutoCloseable {
             try (Transaction tx = session.beginTransaction()) {
                 tx.run("MATCH (a:Document)-[r:" + LIKE_BY + "]" +
                                 "->(b:User) WHERE a.id = {docId} AND b.id = {userId} DELETE r",
-                        Values.parameters("docId", docId, "ownerId", userId));
+                        Values.parameters("docId", docId, "userId", userId));
                 tx.success();  // Mark this write as successful.
             }
         }
